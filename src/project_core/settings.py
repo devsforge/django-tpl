20bb968ce19173965ddbc0ff9d55e4cd2b75846c
Serhii Horodilov <shorodilov@gmail.com>
2025-11-01 01:45:54 +0200

    migrate database to PostgreSQL and update dependencies and configs

--- src/project_core/settings.py
+++ src/project_core/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = "project_core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "NAME": "django",
+        "USER": "django",
+        "PASSWORD": "django",
+        "TEST": {
+            "NAME": "django_test",
+        }
     }
 }
 
